e609eec269072b146ee4469d19f15d3029705024
dawe <dawedawe@gmx.de>
2015-04-07 12:37:18 +0200

    use concurrent.futures for parallel asynchronous feed fetching

--- app/Feed.py
+++ app/Feed.py
@@ -5,9 +5,9 @@ import xml.etree.ElementTree as ET
 
 class Feed():
 
-    def __init__(self, url, proxyurl):
+    def __init__(self, url, rawfeed):
         self.url = url
-        self.proxyurl = proxyurl
+        self.rawfeed = rawfeed
 
     @staticmethod
     def parse_encoding(xmlstr):
@@ -19,22 +19,21 @@ class Feed():
             m2 = re.search('"|\'', xmlstr[m1.end():])
             return xmlstr[m1.end():(m1.end() + m2.start())]
 
-    def get_raw(self):
-        if self.proxyurl is not None:
-            prox_handler = urllib.request.ProxyHandler({'http': self.proxyurl,
-                                                        'https': self.proxyurl})
+    @staticmethod
+    def get_raw(proxyurl, url):
+        if proxyurl is not None:
+            prox_handler = urllib.request.ProxyHandler({'http': proxyurl,
+                                                        'https': proxyurl})
             opener = urllib.request.build_opener(prox_handler)
             urllib.request.install_opener(opener)
-        response = urllib.request.urlopen(self.url)
+        response = urllib.request.urlopen(url)
         return response.read()
 
     def get_channels(self):
         try:
-            rawfeed = self.get_raw()
-            enc = Feed.parse_encoding(rawfeed.decode(errors='ignore'))
-            datastr = rawfeed.decode(enc)
+            enc = Feed.parse_encoding(self.rawfeed.decode(errors='ignore'))
+            datastr = self.rawfeed.decode(enc)
             root = ET.fromstring(datastr)
-
             channels = []
             for channel in root.iter('channel'):
                 title = channel.find('title').text
@@ -44,7 +43,6 @@ class Feed():
                     items.append(Item(item.find('title').text,
                                       item.find('link').text))
                 channels.append(Channel(title, link, items))
-
             return channels
         except Exception as e:
             print('Feed.get_channels() failed for {}'.format(self.url))
--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 from flask import render_template
 from app import app
 from config import FEEDS, PROXY
@@ -7,7 +8,16 @@ from .Feed import Feed
 @app.route('/')
 def home():
     channels = []
-    for l in FEEDS:
-        feed = Feed(l, PROXY)
-        channels.extend(feed.get_channels())
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        future_to_url = {executor.submit(Feed.get_raw, PROXY, url):
+                         url for url in FEEDS}
+        for future in concurrent.futures.as_completed(future_to_url):
+            url = future_to_url[future]
+            try:
+                data = future.result(60)
+                feed = Feed(url, data)
+                channels.extend(feed.get_channels())
+            except Exception as exc:
+                print('fetching feed data failed for {} with {}'.format(url,
+                                                                        exc))
     return render_template('feeds.html', channels=channels)
